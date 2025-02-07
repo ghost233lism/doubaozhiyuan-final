00fd87229cebf97da62ba286b520efd172f3b98b
Modi Jin <2312578@mail.nankai.edu.cn>
2025-02-07 21:53:18 +0800

    添加了“我的”界面

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,8 +1,71 @@
 Page({
   data: {
-    
+    userInfo: null,
+    hasUserInfo: false
   },
-  onLoad: function() {
-    
+
+  onLoad() {
+    // 检查是否已有用户信息
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      });
+    }
+  },
+
+  // 获取用户信息
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '用于完善用户资料',
+      success: (res) => {
+        const userInfo = res.userInfo;
+        wx.setStorageSync('userInfo', userInfo);
+        this.setData({
+          userInfo: userInfo,
+          hasUserInfo: true
+        });
+      },
+      fail: (err) => {
+        console.error('获取用户信息失败：', err);
+        wx.showToast({
+          title: '获取用户信息失败',
+          icon: 'none'
+        });
+      }
+    });
+  },
+
+  // 导航到我的收藏
+  navigateToFavorites() {
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+  },
+
+  // 导航到历史记录
+  navigateToHistory() {
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+  },
+
+  // 导航到意见反馈
+  navigateToFeedback() {
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+  },
+
+  // 导航到关于我们
+  navigateToAbout() {
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
   }
-}) 
+}); 
